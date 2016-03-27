95d832185e008205d220a3ff3cb4a10f2973c199
Andre de Carvalho <andre@Andres-MacBook-Pro.local>
2016-03-27 16:06:21 -0300

    Jogo ja pode ser jogado, porem precisa arrumar o problema de movimento de bordas

--- jogo-dos-8/main.py
+++ jogo-dos-8/main.py
@@ -1,5 +1,5 @@
 from random import shuffle
-CIMA,DIR,ESQ,BAIXO = 0,1,2,3
+CIMA,DIR,ESQ,BAIXO = 'c','d','e','b'
 
 class Game(object):
 
@@ -14,15 +14,15 @@ class Game(object):
                 self.board[i][j] = numeros.pop()
                 if self.board[i][j] is None: self.pos =[i,j]
 
-    def move(direction):
+    def move(self, direction):
         if direction == CIMA:
-            direction = [0,-1]
-        elif direction == BAIXO:
-            direction = [0,1]
-        elif direction == DIR:
             direction = [-1,0]
-        elif direction == ESQ:
+        elif direction == BAIXO:
             direction = [1,0]
+        elif direction == DIR:
+            direction = [0,1]
+        elif direction == ESQ:
+            direction = [0,-1]
         x,y= self.pos
         i,j = direction
         try:
@@ -32,10 +32,35 @@ class Game(object):
             return
         self.board[x][y] = temp
         self.board[x+i][y+j] = None
+        self.pos = [x+i,y+j]
+
+    def check_game_over(self):
+        if self.board == [[1,2,3],[4,5,6],[7,8,None]]:
+            return True
+        return False
+
+    def print_board(self):
+        for line in self.board:
+            print line
+
+    def get_player_input(self):
+        _inp = raw_input('Escolha uma direcao para mover o None ((c)ima,(b)aixo,(d)ireita,(e)squerda) ou (Q) para sair ')
+        return _inp
+
+    def game_loop(self):
+        while True:
+            self.print_board()
+            _inp = self.get_player_input()
+            if _inp == 'Q' or _inp == 'q':
+                print 'saindo...'
+                return
+            self.move(_inp)
+            if self.check_game_over():
+                print 'Voce venceu o jogo!'
+                return
 
 if __name__ == '__main__':
     jogo = Game()
-    for line in jogo.board:
-        print line
+    jogo.game_loop()
 
 
